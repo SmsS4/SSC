6db38e41e5f479e496e28ab6c8212a853125c36f
M Sadegh <sadeghclan93@gmail.com>
2022-02-18 11:40:07 +0330

    extract blogData implemented

--- frontend/src/Blog.js
+++ frontend/src/Blog.js
@@ -6,18 +6,40 @@ import React from 'react';
 import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 import { appendStrUrl } from './StrApi';
 
+function getMediaUrl(media) {
+    return media.attributes.formats.small.url;
+}
+
 function getBlogData(item) {
     // TODO: extract data from 'item' object
+    console.log("Blog post data... here:");
+    console.log(item);
+    const attrs = item.data.attributes;
+    const blocks = attrs.blocks;
+    // for (const block of blocks) {
+    //     console.log(block);
+    //     if (block.media.data != null) {
+    //         console.log("salam")
+    //     } else {
+    //         console.log("media nadarad!")
+    //     }
+    // }
+
     return {
-        blog_title: 'TITLE',
+        blog_title: attrs.title,
+        time: attrs.publishedAt,
+        blocks: attrs.blocks,
     }
 }
 
 function BlogPost({item}) {
     const {
         blog_title,
+        time,
+        blocks,
     } = getBlogData(item);
 
+    // console.log(getMediaUrl(blocks[1].media.data[0]))
     // TODO: render blog data
     return (
         <div>
